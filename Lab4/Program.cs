d11df03c16bd706912f6ee61a1070cf2819cb797
Mosikov <122563061+Mosikov@users.noreply.github.com>
2024-12-30 02:06:32 +0300

    Add files via upload

---
+++ Lab4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            void Sort()
+            {
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 2 == 1)
+                    {
+                        Console.Write(arr[i]+" ");
+                    }
+                }
+            }
+            Sort();
+        }
+    }
+}
